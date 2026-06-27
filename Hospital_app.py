2e6ec91859ad159fd36b5923566ce3ce51bbd112
uare-netizen <arthurbintang135@gmail.com>
2026-06-27 12:21:12 +0700

    Update Hospital_app.py

--- Hospital_app.py
+++ Hospital_app.py
@@ -58,29 +58,29 @@ DEPT_INFO = {
         'next': ['Visit Level 2, Wing B','Estimated wait: 15-25 min','please wear a mask']
     },
     'Cardiology' : {
-        'icon':'❤️','color':'#dc2626','bg':'#fee2e2','border':'#fca5a5',
-        'desc':'Specialises in heart and cardiovascular conditions.'
-        'next': ['Visit Level 3, Wing A','Estimated wait: 20-30 min','Bring any previus ECG reports']
+        'icon' :'❤️','color':'#dc2626','bg':'#fee2e2','border':'#fca5a5',
+        'desc' :'Specialises in heart and cardiovascular conditions.'
+        'next' : ['Visit Level 3, Wing A','Estimated wait: 20-30 min','Bring any previus ECG reports']
     },
     'Gastroenterology' : {
-        'icon':'🦵','color':'##d97706','bg':'#fef3c7','border':'#fcd3d',
-        'desc':'Specialises in digestive system and abdominal conditions.'
-        'next': ['Visit Level 1, Wing C','Estimated wait: 10-20 min','Avoid Eating before consultation']
+        'icon' :'🦵','color':'##d97706','bg':'#fef3c7','border':'#fcd3d',
+        'desc' :'Specialises in digestive system and abdominal conditions.'
+        'next' : ['Visit Level 1, Wing C','Estimated wait: 10-20 min','Avoid Eating before consultation']
     },
     'Neurology' : {
-        'icon':'🧠','color':'#7c3aed','bg':'#efe9fe','border':'#c4b5fd',
-        'desc':'Specialises in disgestive system and abdominal conditions.'
-        'next': ['Visit Level 4, Wing A','Estimated wait: 25-30 min','Brint list of current medications']
+        'icon' :'🧠','color':'#7c3aed','bg':'#efe9fe','border':'#c4b5fd',
+        'desc' :'Specialises in disgestive system and abdominal conditions.'
+        'next' : ['Visit Level 4, Wing A','Estimated wait: 25-30 min','Brint list of current medications']
     },
       'General Machine' : {
-        'icon':'🩺','color':'#059669','bg':'#d1fae5','border':'#6ee7b7',
-        'desc':'Handles general health concerns and non-specialist conditions.'
-        'next': ['Visit Level 1, Wing A','Estimated wait: 10-15 min','Registration desk is open 24/7']
+        'icon' :'🩺','color':'#059669','bg':'#d1fae5','border':'#6ee7b7',
+        'desc' :'Handles general health concerns and non-specialist conditions.'
+        'next' : ['Visit Level 1, Wing A','Estimated wait: 10-15 min','Registration desk is open 24/7']
     },
     'Dermatology' : {
-        'icon':'🩸','color':'#b45309','bg':'#fef9c3','border':'#fde68a',
-        'desc':'Specialises in skin, hair, and nail condition.'
-        'next': ['Visit Level 2, Wing D','Estimated wait: 15-20 min','Bring photos of affected area if possible']
+        'icon' :'🩸','color':'#b45309','bg':'#fef9c3','border':'#fde68a',
+        'desc' :'Specialises in skin, hair, and nail condition.'
+        'next' : ['Visit Level 2, Wing D','Estimated wait: 15-20 min','Bring photos of affected area if possible']
     },
 }
 #-- Hero Header --
