cc941725c7830421dc080005b045e6fa348aa357
uare-netizen <arthurbintang135@gmail.com>
2026-06-27 12:23:40 +0700

    Update Hospital_app.py

--- Hospital_app.py
+++ Hospital_app.py
@@ -54,12 +54,12 @@ cc_map        = bundle['cc_map']
 DEPT_INFO = {
     'Respiratory Medicine' : {
         'icon':'🫁','color':'#0284c7','bg':'#e0f2fe','border':'#7dd3fc',
-        'desc':'Specialises in conditions affecting the lungs and airways.'
+        'desc':'Specialises in conditions affecting the lungs and airways.',
         'next':['Visit Level 2, Wing B','Estimated wait: 15-25 min','please wear a mask']
     },
     'Cardiology' : {
         'icon':'❤️','color':'#dc2626','bg':'#fee2e2','border':'#fca5a5',
-        'desc':'Specialises in heart and cardiovascular conditions.'
+        'desc':'Specialises in heart and cardiovascular conditions.',
         'next':['Visit Level 3, Wing A','Estimated wait: 20-30 min','Bring any previus ECG reports']
     },
     'Gastroenterology' : {
@@ -69,17 +69,17 @@ DEPT_INFO = {
     },
     'Neurology' : {
         'icon':'🧠','color':'#7c3aed','bg':'#efe9fe','border':'#c4b5fd',
-        'desc':'Specialises in disgestive system and abdominal conditions.'
+        'desc':'Specialises in disgestive system and abdominal conditions.',
         'next':['Visit Level 4, Wing A','Estimated wait: 25-30 min','Brint list of current medications']
     },
       'General Machine' : {
         'icon':'🩺','color':'#059669','bg':'#d1fae5','border':'#6ee7b7',
-        'desc':'Handles general health concerns and non-specialist conditions.'
+        'desc':'Handles general health concerns and non-specialist conditions.',
         'next':['Visit Level 1, Wing A','Estimated wait: 10-15 min','Registration desk is open 24/7']
     },
     'Dermatology' : {
         'icon':'🩸','color':'#b45309','bg':'#fef9c3','border':'#fde68a',
-        'desc':'Specialises in skin, hair, and nail condition.'
+        'desc':'Specialises in skin, hair, and nail condition.',
         'next':['Visit Level 2, Wing D','Estimated wait: 15-20 min','Bring photos of affected area if possible']
     },
 }
